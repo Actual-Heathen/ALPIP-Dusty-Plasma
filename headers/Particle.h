e3c11d7ba83c7a94d40ad410d5d2a39a84c43e20
Actual_Heathen <tonyyluis@hotmail.com>
2022-06-02 18:48:11 -0500

    added velocity and acceleration to particle

--- headers/Particle.h
+++ headers/Particle.h
@@ -2,12 +2,24 @@
 
 class Particle
 {
+    struct Vector
+    {
+        float xVec = 0;
+        float yVec = 0;
+        float mag = 0;
+    };
+
     public:
         Particle();
         Particle(float x, float y, float m);
         float getX();
         float getY();
         float getMass();
+        float getSpeed();
+        void addAcceleration(float x, float y);
+        void move(float s);
+        void resetAcc();
+        void resetVel();
     private:
         void setX(float x);
         void setY(float y);
@@ -16,5 +28,10 @@ class Particle
         float x_pos;
         float y_pos;
         float mass;
-        float velocity;
+        float speed = 0;
+        float velX = 0;
+        float velY = 0;
+        float accel = 0;
+        float accelX = 0;
+        float accelY = 0;
 };
--- source/Particle.cpp
+++ source/Particle.cpp
@@ -1,4 +1,5 @@
 #include "../headers/Particle.h"
+#include <math.h>
 
 Particle::Particle()
 {
@@ -42,4 +43,47 @@ float Particle::getY()
 float Particle::getMass()
 {
     return mass;
+}
+float Particle::getSpeed()
+{
+    return speed;
+}
+
+void Particle::addAcceleration(float x, float y)
+{
+    accelX += x;
+    accelY += y;
+    accel = sqrt(pow(x,2) + pow(y,2));
+}
+
+void Particle::move(float s)
+{
+    float dX = velX*s + .5*accelX*s*s;
+    float dY = velY*s + .5*accelY*s*s;
+
+    velX = dX;
+    velY = dY;
+
+    float xV = dX/s;
+    float yV = dY/s;
+    speed = sqrt(xV*xV + yV*yV);
+
+    x_pos += dX;
+    y_pos += dY;
+
+    resetAcc();
+}
+
+void Particle::resetAcc()
+{
+    accelX = 0;
+    accelY = 0;
+    accel = 0;
+}
+
+void Particle::resetVel()
+{
+    velX = 0;
+    velY = 0;
+    speed = 0;
 }
