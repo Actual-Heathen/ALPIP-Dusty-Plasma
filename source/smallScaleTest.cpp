049a6b4f6cac137ec61f7f2c1e228580f0b3f886
Actual_Heathen <tonyyluis@hotmail.com>
2022-06-03 21:47:54 -0500

    MASSIVE CHANGES

--- source/smallScaleTest.cpp
+++ source/smallScaleTest.cpp
@@ -4,38 +4,90 @@
 #include <math.h>
 #include "../headers/Particle.h"
 #include "../headers/Point.h"
+#include <vector>
 
 using namespace std;
-
+#define paticleCount 1
+//#define pointCount 2
+#define spacing .00001
+#define gridDiv 3
 
 int main()
 {
     srand(time(NULL));
 
-    Particle dust[3];
-    Point points[4];
+    vector<Particle> dust;
+    //Point points[pointCount];
 
-    points[0] = Point(0.0f,0.0f);
-    points[1] = Point(0.0f,1.0f);
-    points[2] = Point(1.0f,0.0f);
-    points[3] = Point(1.0f,1.0f);
+    double rho[gridDiv][gridDiv];
 
-    for (int i = 0; i < 3; i++)
+
+    for (int i = 0; i < paticleCount; i++) //set particle position
     {
-        Particle temp(float(rand())/float(RAND_MAX), float(rand())/float(RAND_MAX),1.0);
+        Particle temp( (double(rand())/double(RAND_MAX)*spacing*(gridDiv-1)), (double(rand())/double(RAND_MAX))*spacing*(gridDiv-1),1.0);
 
-        dust[i] = temp;
+        dust.push_back(temp);
+        //cout << dust[i].getX() << ", " << dust[i].getY() << "\n";
     }
 
-    for (int i = 0; i < 3; i++)
+    for (int l = 0; l< 600; l++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int i = 0; i < gridDiv; i++)   //initialize grid
         {
-            points[j].addGravity(dust[i]);
-            cout << "point " << j <<", paticle " << i << ":\n";
-            cout <<  "(x,y): (" << dust[i].getX() << ", " << dust[i].getY() << ")\n";
-            cout << "gravity: " << points[j].getGVM() << "\n\n";
+            for (int j = 0; j < gridDiv; j++)
+            {
+                rho[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < paticleCount; i++) //calculate rho
+        {
+            int iXm = floor(dust[i].getX()/spacing);
+            int iXp = iXm + 1;
+            double wXm = 1- abs((dust[i].getX()-iXm*spacing)/spacing);
+            double wXp = 1- abs((dust[i].getX()-iXp*spacing)/spacing);
+
+            int iYm = floor(dust[i].getY()/spacing);
+            int iYp = iYm + 1;
+            double wYm = 1- abs((dust[i].getY()-iYm*spacing)/spacing);
+            double wYp = 1- abs((dust[i].getY()-iYp*spacing)/spacing);
+            
+            //cout << iXp<< ", "<< wXp << "<-ixp "<< iYp << ", "<< wYp << "<-iyp\n";
+            //cout << iXm<< " ,"<< wXm << "<-ixm "<< iYm << ", "<< wYm << "<-iym\n";
+
+            rho[iXm][iYm] += wXm*wYm;
+            rho[iXm][iYp] += wXm*wYp;
+            rho[iXp][iYm] += wXp*wYm;
+            rho[iXp][iYp] += wXp*wYp;
+        }
+
+        double rhoTemp = 0;
+
+        for (int i = 0; i < gridDiv; i++)
+        {
+            for (int j = 0; j < gridDiv; j++)
+            {
+                //cout << rho[i][j] << "\n";
+                rhoTemp += rho[i][j];
+            }
+        }
+
+        //cout << rhoTemp << " rho sum\n";
+
+        for (int i = 0; i < paticleCount; i++) //calculate gravity
+        {
+            dust[i].addAcceleration(rho,spacing);
+            dust[i].move(1);
+            if (i == 0)
+                cout<< dust[i].getX() << ", " << dust[i].getY() << "\n ";
+            //if (i == 1)
+            //    cout<< dust[i].getX() << ", " << dust[i].getY() << "\n ";
+            // if (i == 2)
+            //     cout<< dust[i].getX() << ", " << dust[i].getY() << ", ";
+            // if (i == 3)
+            //     cout<< dust[i].getX() << ", " << dust[i].getY() << ", ";
+            // if (i == 4)
+            //     cout<< dust[i].getX() << ", " << dust[i].getY() << "\n";
         }
     }
-
 }
